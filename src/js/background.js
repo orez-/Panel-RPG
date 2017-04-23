6baa8f8f1a9502f961294efa08a3eb38a4b92e73
Brian Shaginaw <brian.shaginaw@warbyparker.com>
2017-04-23 18:35:30 -0400

    Get dat infinite scroll going.
    But grimsby six screens
    five layers of parallax
    ok tha- maybe more
    ok thanks grimsby
    
    Imagine.

--- src/js/background.js
+++ src/js/background.js
@@ -2,9 +2,32 @@ window.myGame = window.myGame || {};
 
 (function(Phaser, myGame) {
     const Background = function (game) {
-        Phaser.Sprite.call(this, game, 0, 21, 'background');
+        Phaser.Group.call(this, game);
+        this.x = 0;
+        this.y = 21;
+        this.leftPanel = this.add(new BackgroundSegment(game, 0));
+        this.rightPanel = this.add(new BackgroundSegment(game, 1));
+
+        game.time.events.loop(40, this.advanceScene, this);
     };
-    Background.prototype = Object.create(Phaser.Sprite.prototype);
+    Background.prototype = Object.create(Phaser.Group.prototype);
     Background.prototype.constructor = Background;
+
+    Background.prototype.advanceScene = function () {
+        this.leftPanel.x -= 1;
+        this.rightPanel.x -= 1;
+        if (this.rightPanel.x <= 0) {
+            this.leftPanel.x += 200;
+            [this.leftPanel, this.rightPanel] = [this.rightPanel, this.leftPanel];
+        }
+    };
+
+    // ---
+
+    const BackgroundSegment = function (game, x) {
+        Phaser.Sprite.call(this, game, x * 200, 0, 'background');
+    };
+    BackgroundSegment.prototype = Object.create(Phaser.Sprite.prototype);
+    BackgroundSegment.prototype.constructor = BackgroundSegment;
     myGame.Background = Background;
 })(window.Phaser, window.myGame);
