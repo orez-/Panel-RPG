a1a4ce5c3851e441c32d3615436ce2b0d4e67d57
Brian Shaginaw <brian.shaginaw@warbyparker.com>
2017-04-23 15:50:03 -0400

    Radio the buttons.

--- src/js/button.js
+++ src/js/button.js
@@ -1,11 +1,10 @@
 window.myGame = window.myGame || {};
 
-
 const NUM_ICONS = 5;
 (function(Phaser, myGame) {
 
-    const Button = function (game, x, y, buttonId) {
-        Phaser.Sprite.call(this, game, x, y, 'button');
+    const Button = function (game, x, buttonId) {
+        Phaser.Sprite.call(this, game, x * 19, 0, 'button');
 
         this.frameColumn = buttonId;
         this.frame = this.frameColumn;
@@ -17,11 +16,39 @@ const NUM_ICONS = 5;
     Button.prototype.constructor = Button;
 
     Button.prototype.up = function () {
+        this.parent.setSelected(this);
         this.frame = this.frameColumn + NUM_ICONS * 2;
     };
 
     Button.prototype.down = function () {
         this.frame = this.frameColumn + NUM_ICONS;
-    }
+    };
+
+    Button.prototype.deselect = function () {
+        this.frame = this.frameColumn;
+    };
+
+    const ButtonGroup = function (game, x, y) {
+        Phaser.Group.call(this, game);
+        this.x = x;
+        this.y = y;
+
+        this.add(new myGame.Button(game, 0, 1));
+        this.add(new myGame.Button(game, 1, 4));
+        this.add(new myGame.Button(game, 2, 3));
+        this.add(new myGame.Button(game, 3, 2));
+        this.selected = null;
+    };
+    ButtonGroup.prototype = Object.create(Phaser.Group.prototype);
+    ButtonGroup.prototype.constructor = ButtonGroup;
+
+    ButtonGroup.prototype.setSelected = function (button) {
+        if (this.selected) {
+            this.selected.deselect();
+        }
+        this.selected = button;
+    };
+
     myGame.Button = Button;
+    myGame.ButtonGroup = ButtonGroup;
 })(window.Phaser, window.myGame);
--- src/js/panel.js
+++ src/js/panel.js
@@ -19,14 +19,7 @@ window.myGame = window.myGame || {};
         icon.frame = playerId + 6;
         this.add(icon);
 
-        var button = new myGame.Button(game, 178, 0, 2);
-        this.add(button);
-        button = new myGame.Button(game, 178 - 19, 0, 4);
-        this.add(button);
-        button = new myGame.Button(game, 178 - 38, 0, 3);
-        this.add(button);
-        button = new myGame.Button(game, 178 - 57, 0, 1);
-        this.add(button);
+        this.add(new myGame.ButtonGroup(game, 122, 0));
     };
     Panel.prototype = Object.create(Phaser.Group.prototype);
     Panel.prototype.constructor = Panel;
